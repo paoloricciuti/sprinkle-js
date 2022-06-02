cbc0b449faa471d3a307cfe7b20e4a8f155731ed
Paolo Ricciuti <ricciutipaolo@gmail.com>
2022-06-02 23:26:36 +0200

    Update gitignore to remove cjs

---
+++ dist/declarations/src/index.d.ts
@@ -0,0 +1,14 @@
+import { AppendNode, ICreateEffect, IEffect, IStringOrDomElement } from "./types/index";
+declare const createVariable: <T extends Object>(value: T) => T;
+declare const createRef: (ref: any) => {
+    value: any;
+};
+declare const createEffect: ICreateEffect;
+declare const untrack: (fn: () => any) => any;
+declare const bindTextContent: <TElement extends HTMLElement = HTMLElement>(domElement: IStringOrDomElement<TElement>, fn: IEffect<string, TElement>) => TElement | null;
+declare const bindClass: <TElement extends HTMLElement = HTMLElement>(domElement: IStringOrDomElement<TElement>, className: string, fn: IEffect<boolean, TElement>) => TElement | null;
+declare const bindInputValue: (domElement: IStringOrDomElement<HTMLInputElement>, fn: IEffect<string, HTMLInputElement>) => HTMLInputElement | null;
+declare const bindDom: <TElement extends HTMLElement = HTMLElement>(domElement: IStringOrDomElement<TElement>, fn: IEffect<any, TElement>) => TElement | null;
+declare const bindStyle: <TElement extends HTMLElement = HTMLElement>(domElement: IStringOrDomElement<TElement>, fn: IEffect<any, TElement>) => TElement | undefined;
+declare const bindChildrens: <TElement extends HTMLElement = HTMLElement>(domElement: IStringOrDomElement<TElement>, fn: IEffect<NodeListOf<AppendNode<ChildNode>>, TElement>) => TElement | null;
+export { createEffect, untrack, createRef, createVariable, bindInputValue, bindTextContent, bindDom, bindClass, bindStyle, bindChildrens };
---
+++ dist/declarations/src/utils.d.ts
@@ -0,0 +1,9 @@
+import { IGetDomElementFn } from "./types/index";
+declare const getDomElement: IGetDomElementFn;
+declare const updateDom: (domElement: any, properties: any, namespace?: string[]) => void;
+declare const diff: <T>(arr?: T[], arr2?: T[], eq?: (a: T, b: T) => boolean) => {
+    type: string;
+    value: T;
+}[];
+declare const findNext: <T>(arr: T[], eq: (element: T, index: number, array: T[]) => boolean, index?: number) => T | undefined;
+export { getDomElement, findNext, diff, updateDom, };
