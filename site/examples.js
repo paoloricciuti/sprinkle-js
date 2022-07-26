adfd1ad7215ad7ea8d024a6ba7e9c0d958bbdddd
Paolo Ricciuti <ricciutipaolo@gmail.com>
2022-07-26 11:31:19 +0200

    Small landing page modifications

--- site/examples.js
+++ site/examples.js
@@ -4,10 +4,14 @@ export default [
         description: "A drawing board that shows the power of arrays and bindChildrens",
     }, {
         pen: "gOebVrw",
-        description: "Can Sprinkle JS store HTMLELments? Sure you can! This examples shows when it's useful.",
+        description: "Can you store HTMLELments in a Sprinkle JS variable? Sure you can! This examples shows when it's useful.",
     },
     {
         pen: "rNJgJdo",
         description: "This example shows how SprinkleJS can help you write simple games like Tic-Tac-Toe!",
     },
+    {
+        pen: "qBoXGwZ",
+        description: "Can a list of examples not include a TODO app? I don't think so."
+    }
 ];
--- site/index.js
+++ site/index.js
@@ -1,32 +1,42 @@
-import { createVariable, bindInputValue, bindClass, bindDom, bindTextContent, bindStyle, createEffect } from "https://cdn.skypack.dev/sprinkle-js";
+import { bindChildrens, bindDom, bindStyle, bindTextContent, createVariable } from "https://cdn.skypack.dev/sprinkle-js";
 import examples from "./examples.js";
 
 const getStartedSection = document.getElementById("get-started");
-
-examples.forEach(example => {
+if (window.__CPEmbed) {
+    examples.forEach(example => {
+        const article = document.createElement("article");
+        article.classList.add("code-example");
+        article.innerHTML = `<p class="codepen" data-border="none" data-height="300" data-theme-id="dark" data-default-tab="js,result"
+        data-slug-hash="${example.pen}" data-editable="true" data-user="paoloricciuti"
+        style="height: 300px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+        <span>See the Pen <a href="https://codepen.io/paoloricciuti/pen/${example.pen}">
+                Bubbles</a> by paoloricciuti (<a href="https://codepen.io/paoloricciuti">@paoloricciuti</a>)
+            on <a href="https://codepen.io">CodePen</a>.</span>
+    </p>
+    <p>
+    ${example.description}
+    </p>`;
+        getStartedSection.append(article);
+    });
+    window.__CPEmbed();
+} else {
     const article = document.createElement("article");
-    article.classList.add("code-example");
-    article.innerHTML = `<p class="codepen" data-border="none" data-height="300" data-theme-id="dark" data-default-tab="js,result"
-    data-slug-hash="${example.pen}" data-editable="true" data-user="paoloricciuti"
-    style="height: 300px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
-    <span>See the Pen <a href="https://codepen.io/paoloricciuti/pen/${example.pen}">
-            Bubbles</a> by paoloricciuti (<a href="https://codepen.io/paoloricciuti">@paoloricciuti</a>)
-        on <a href="https://codepen.io">CodePen</a>.</span>
-</p>
-<p>
-${example.description}
-</p>`;
+    article.classList.add("no-examples");
+    article.innerHTML = `
+        There should have been Codepen examples there but apperently there's no Codepen script in the page. Try to reload the page or visit <a href="https://codepen.io/paoloricciuti">@paoloricciuti</a> for some examples.
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 138 26" fill="none" stroke="#000" stroke-width="2.3" stroke-linecap="round" stroke-linejoin="round">
+  <path d="M80 6h-9v14h9 M114 6h-9 v14h9 M111 13h-6 M77 13h-6 M122 20V6l11 14V6 M22 16.7L33 24l11-7.3V9.3L33 2L22 9.3V16.7z M44 16.7L33 9.3l-11 7.4 M22 9.3l11 7.3 l11-7.3 M33 2v7.3 M33 16.7V24 M88 14h6c2.2 0 4-1.8 4-4s-1.8-4-4-4h-6v14 M15 8c-1.3-1.3-3-2-5-2c-4 0-7 3-7 7s3 7 7 7 c2 0 3.7-0.8 5-2 M64 13c0 4-3 7-7 7h-5V6h5C61 6 64 9 64 13z"/>
+</svg>
+    `;
     getStartedSection.append(article);
-});
-
-window.__CPEmbed();
+}
 
 const state = createVariable({
     logoRotation: 0,
     title: "well nothing!",
-    theme: "auto",
     mineBitcoin: true,
     bitcoinRate: 10,
+    bitcoinMined: 0,
 }, {
     bitcoinRate: () => false,
 });
@@ -62,6 +72,7 @@ arms.addEventListener("animationiteration", () => {
                 bitcoinElem.remove();
             });
             document.querySelector(".footer").append(bitcoinElem);
+            state.bitcoinMined++;
         }
     }
     iterationCount++;
@@ -77,8 +88,9 @@ const df = document.createDocumentFragment();
 df.append(openPar);
 for (let key of keys) {
     let div = document.createElement("div");
-    let span = document.createElement("span");
-    span.textContent = `"${key}":`;
+    let label = document.createElement("label");
+    label.textContent = `"${key}":`;
+    label.htmlFor = key;
     let spanVal = document.createElement("span");
     const type = typeof state[key];
     if (type === "string" || type === "number") {
@@ -87,6 +99,7 @@ for (let key of keys) {
         const input = document.createElement("input");
         input.type = type === "string" ? "text" : "number";
         input.className = "mod-value";
+        input.id = key;
         bindDom(input, () => ({
             value: state[key],
             style: {
@@ -103,21 +116,25 @@ for (let key of keys) {
             spanVal.append(input);
         }
     } else if (type === "boolean") {
-        spanVal.textContent = state[key];
-        const checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        bindDom(checkbox, () => ({
-            checked: state[key],
-        }));
-        checkbox.addEventListener("change", (e) => {
-            const modifier = stateModifiers[key] ?? ((val) => val);
-            state[key] = modifier(e.target.checked);
+        bindChildrens(spanVal, () => `${state[key]}<input type="checkbox" key="${key}" id="${key}" />`, (_, objects) => {
+            const checkbox = objects.get(key);
+            if (checkbox) {
+                bindDom(checkbox.element, () => ({
+                    checked: state[key],
+                }));
+                if (checkbox.isNew) {
+                    checkbox.element.addEventListener("change", (e) => {
+                        const modifier = stateModifiers[key] ?? ((val) => val);
+                        state[key] = modifier(e.target.checked);
+                    });
+                }
+            }
+
         });
-        spanVal.append(checkbox);
     }
-    span.className = "key";
+    label.className = "key";
     spanVal.className = "value";
-    div.append(span);
+    div.append(label);
     div.append(spanVal);
     df.append(div);
 }
