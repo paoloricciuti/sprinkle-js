1faffa9db8c69b792d2472d6d9f3e711adeed7ef
Paolo Ricciuti <ricciutipaolo@gmail.com>
2022-08-24 12:38:07 +0200

    Removed preconstruct in favor of vite

--- dist/declarations/src/index.d.ts
+++ dist/declarations/src/index.d.ts
@@ -1,24 +1,24 @@
 import { CSSStyles, DiffedElements, DOMUpdate, ICreateEffect, ICssVariable, IEffect, IEqualFunction, IEqualFunctionMap, IStringOrDomElement, Primitive } from "./types/index";
 declare const batch: (fn: Function) => void;
-declare const createVariable: <T extends object>(value: T, eq?: IEqualFunctionMap<T> | undefined) => T;
-declare const createCssVariable: <T extends ICssVariable, E extends HTMLElement = HTMLHtmlElement>(value: T, eq?: IEqualFunctionMap<T> | undefined, root?: IStringOrDomElement<E>) => T;
-declare const createComputed: <T>(fn: () => T, eq?: IEqualFunction<T> | undefined) => {
+declare const createVariable: <T extends object>(value: T, eq?: IEqualFunctionMap<T>) => T;
+declare const createCssVariable: <T extends ICssVariable, E extends HTMLElement = HTMLHtmlElement>(value: T, eq?: IEqualFunctionMap<T>, root?: IStringOrDomElement<E>) => T;
+declare const createComputed: <T>(fn: () => T, eq?: IEqualFunction<T>) => {
     value: T;
 };
-declare const createStored: <T extends Object>(key: string, value: T, eq?: IEqualFunctionMap<T> | undefined, storage?: Storage) => T;
-declare const createRef: <T extends Primitive>(ref: T, eq?: IEqualFunction<T> | undefined) => {
+declare const createStored: <T extends Object>(key: string, value: T, eq?: IEqualFunctionMap<T>, storage?: Storage) => T;
+declare const createRef: <T extends Primitive>(ref: T, eq?: IEqualFunction<T>) => {
     value: T;
 };
 declare const createEffect: ICreateEffect;
 declare const untrack: (fn: () => any) => any;
-declare const bindTextContent: <TElement extends HTMLElement = HTMLElement>(domElement: IStringOrDomElement<TElement>, fn: IEffect<string, TElement>) => TElement | null;
-declare const bindInnerHTML: <TElement extends HTMLElement = HTMLElement>(domElement: IStringOrDomElement<TElement>, fn: IEffect<string, TElement>) => TElement | null;
-declare const bindClass: <TElement extends HTMLElement = HTMLElement>(domElement: IStringOrDomElement<TElement>, className: string, fn: IEffect<boolean, TElement>) => TElement | null;
+declare const bindTextContent: <TElement extends HTMLElement = HTMLElement>(domElement: IStringOrDomElement<TElement>, fn: IEffect<string, TElement>) => TElement;
+declare const bindInnerHTML: <TElement extends HTMLElement = HTMLElement>(domElement: IStringOrDomElement<TElement>, fn: IEffect<string, TElement>) => TElement;
+declare const bindClass: <TElement extends HTMLElement = HTMLElement>(domElement: IStringOrDomElement<TElement>, className: string, fn: IEffect<boolean, TElement>) => TElement;
 declare const bindClasses: <TElement extends HTMLElement = HTMLElement>(domElement: IStringOrDomElement<TElement>, fn: IEffect<{
     [key: string]: boolean;
-}, TElement>) => TElement | null;
-declare const bindInputValue: (domElement: IStringOrDomElement<HTMLInputElement>, fn: IEffect<string, HTMLInputElement>) => HTMLInputElement | null;
-declare const bindDom: <TElement extends HTMLElement = HTMLElement>(domElement: IStringOrDomElement<TElement>, fn: IEffect<DOMUpdate<TElement>, TElement>) => TElement | null;
-declare const bindStyle: <TElement extends HTMLElement = HTMLElement>(domElement: IStringOrDomElement<TElement>, fn: IEffect<CSSStyles<TElement>, TElement>) => TElement | undefined;
-declare const bindChildrens: <TElement extends HTMLElement = HTMLElement>(domElement: IStringOrDomElement<TElement>, fn: IEffect<string, TElement>, afterDiff?: ((root: TElement, elements: Map<string, DiffedElements>) => void) | undefined) => TElement | null;
+}, TElement>) => TElement;
+declare const bindInputValue: (domElement: IStringOrDomElement<HTMLInputElement>, fn: IEffect<string, HTMLInputElement>) => HTMLInputElement;
+declare const bindDom: <TElement extends HTMLElement = HTMLElement>(domElement: IStringOrDomElement<TElement>, fn: IEffect<DOMUpdate<TElement>, TElement>) => TElement;
+declare const bindStyle: <TElement extends HTMLElement = HTMLElement>(domElement: IStringOrDomElement<TElement>, fn: IEffect<CSSStyles<TElement>, TElement>) => TElement;
+declare const bindChildrens: <TElement extends HTMLElement = HTMLElement>(domElement: IStringOrDomElement<TElement>, fn: IEffect<string, TElement>, afterDiff?: (root: TElement, elements: Map<string, DiffedElements>) => void) => TElement;
 export { createEffect, untrack, batch, createRef, createVariable, createCssVariable, createComputed, createStored, bindInputValue, bindInnerHTML, bindTextContent, bindDom, bindClass, bindClasses, bindStyle, bindChildrens };
--- dist/declarations/src/utils.d.ts
+++ dist/declarations/src/utils.d.ts
@@ -1,14 +1,10 @@
 import { IGetDomElementFn } from "./types/index";
 declare const getDomElement: IGetDomElementFn;
 declare const updateDom: (domElement: any, properties: any, namespace?: string[]) => void;
-declare const diff: <T>(arr?: T[], arr2?: T[], eq?: (a: T, b: T) => boolean) => {
-    type: string;
-    value: T;
-    skip: boolean;
-}[];
-declare const findNext: <T>(arr: T[], eq: (element: T, index: number, array: T[]) => boolean, index?: number) => T | undefined;
-declare const getRawType: (obj: unknown) => string;
+declare const diff: <T>(arr?: T[], arr2?: T[], eq?: (a: T, b: T) => boolean) => any[];
+declare const findNext: <T>(arr: T[], eq: (element: T, index: number, array: T[]) => boolean, index?: number) => T;
+declare const getRawType: (obj: unknown) => any;
 declare const html: (innerHTML: string) => DocumentFragment;
-declare const attribute: (element: Element, attribute: string) => string | null;
-declare const key: (element: Node) => string | null;
+declare const attribute: (element: Element, attribute: string) => string;
+declare const key: (element: Node) => string;
 export { getDomElement, findNext, diff, updateDom, getRawType, html, attribute, key, };
