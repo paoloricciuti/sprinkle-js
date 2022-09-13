4efeef2b24ae46005fbefe263224dab4f5c8350b
Paolo Ricciuti <ricciutipaolo@gmail.com>
2022-09-13 19:59:14 +0200

    Better landing page

--- site/index.js
+++ site/index.js
@@ -1,7 +1,7 @@
 import { bindChildrens, bindDom, bindStyle, bindTextContent, createVariable } from "./dist/sprinkle-js.es.js";
 import examples from "./examples.js";
 
-const getStartedSection = document.getElementById("get-started");
+const examplesSection = document.getElementById("examples");
 const codepenScriptTag = document.createElement("script");
 codepenScriptTag.src = "https://cpwebassets.codepen.io/assets/embed/ei.js";
 codepenScriptTag.addEventListener("load", () => {
@@ -19,7 +19,7 @@ codepenScriptTag.addEventListener("load", () => {
         <p>
         ${example.description}
         </p>`;
-            getStartedSection.append(article);
+            examplesSection.append(article);
         });
         window.__CPEmbed();
     } else {
@@ -31,17 +31,17 @@ codepenScriptTag.addEventListener("load", () => {
       <path d="M80 6h-9v14h9 M114 6h-9 v14h9 M111 13h-6 M77 13h-6 M122 20V6l11 14V6 M22 16.7L33 24l11-7.3V9.3L33 2L22 9.3V16.7z M44 16.7L33 9.3l-11 7.4 M22 9.3l11 7.3 l11-7.3 M33 2v7.3 M33 16.7V24 M88 14h6c2.2 0 4-1.8 4-4s-1.8-4-4-4h-6v14 M15 8c-1.3-1.3-3-2-5-2c-4 0-7 3-7 7s3 7 7 7 c2 0 3.7-0.8 5-2 M64 13c0 4-3 7-7 7h-5V6h5C61 6 64 9 64 13z"/>
     </svg>
         `;
-        getStartedSection.append(article);
+        examplesSection.append(article);
     }
 });
 const goOnCodepenObserver = new IntersectionObserver(([entry]) => {
     if (entry.isIntersecting) {
         document.head.append(codepenScriptTag);
-        goOnCodepenObserver.unobserve(getStartedSection);
+        goOnCodepenObserver.unobserve(examplesSection);
     }
 });
 
-goOnCodepenObserver.observe(getStartedSection);
+goOnCodepenObserver.observe(examplesSection);
 
 window.scriptTag = codepenScriptTag;
 
